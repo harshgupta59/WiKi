528230b3efabf9ac72b27771eff3174ee5f9856e
Harsh Gupta <hg.harshgupta1810@gmail.com>
2020-12-20 20:55:41 +0530

    first commit

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,21 @@
 from . import views
 from django.urls import path
-from .views import PostListView,PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView, 
+    PostUpdateView, 
+    PostDeleteView, 
+    UserPostListView,
+    RandomPostListView,
+)
 
 urlpatterns = [ 
     path('',PostListView.as_view(), name='blog-home'),
+    path('random/',RandomPostListView.as_view(), name='random-post'),
+    path('search/',views.SearchView, name='search'),
+    path('announcements/',views.announcements, name='announcements'),
+    path('user/<str:username>',UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/',PostDetailView.as_view(), name='post-detail'),
     path('post/new/',PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/',PostUpdateView.as_view(), name='post-update'),
--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
 from .models import Post
-from django.views.generic import ListView, DetailView,CreateView, UpdateView, DeleteView
+from django.db.models import Q
+from django.views.generic import ListView, DetailView,CreateView, UpdateView, DeleteView, TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-
-def home(request):
-    context= {
-        'posts': Post.objects.all()
-    }
-    return render(request, 'blog/home.html',context)
+import random
 
 class PostListView(ListView):
     model = Post
     template_name="blog/home.html"#<app>/<model>_<view_type>.html
     context_object_name='posts'
     ordering=['-date_posted']
-    paginate_by = 2
+    paginate_by = 5
+
+class RandomPostListView(ListView):
+    model = Post.objects.order_by('?').first()
+    template_name="blog/random.html"#<app>/<model>_<view_type>.html
+    context_object_name='posts'
+    
+    def get_queryset(self):
+        return random.sample(list(Post.objects.all()),2)
+        
+'''
+class SearchView(ListView):
+    model=Post
+    template_name = 'blog/search.html'
+    context_object_name='posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        query = self.kwargs.get('q', '')
+        object_list = self.model.objects.all()
+        return Post.objects.filter(title__icontains=query).order_by('-date_posted')
+'''
+class UserPostListView(ListView):
+    model = Post
+    template_name="blog/user_posts.html"#<app>/<model>_<view_type>.html
+    context_object_name='posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username = self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
+
 
 class PostDetailView(DetailView):
     model = Post
@@ -56,3 +84,17 @@ class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin, DeleteView):
 
 def about(request):
     return render(request, 'blog/about.html',{'title': 'About'})
+
+def announcements(request):
+    return render(request, 'blog/announcements.html',{'title': 'About'})
+
+
+def SearchView(request):
+    queryset=Post.objects.all()
+    query=request.GET.get('q')
+    if query:
+        queryset=queryset.filter(title__icontains=query)
+    return render(request,'blog/search.html',{
+        "posts":queryset
+    })
+
